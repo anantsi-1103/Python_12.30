732359972bd5f0a3655c2dd303e4527eb2cacc8b
Anant <acera52024@gmail.com>
2026-01-22 11:27:32 +0530

    FinalProject

--- Project.py
+++ Project.py
@@ -1,183 +1,177 @@
 import os
 from abc import ABC, abstractmethod
 
-
-#abstract class - Abstraction
+# abstract class
 class Account(ABC):
-
-    def __init__(self, Account_number , owner , balance = 0):
-        self.Account_number = Account_number
+    def __init__(self, account_number, owner, balance=0):
+        self.account_number = account_number
         self.owner = owner
-        self.__balance= balance # encapsulation
+        self.__balance = balance
 
     @abstractmethod
-    def withdraw():
+    def withdraw(self, amt):
         pass
 
     def deposit(self, amt):
         if amt > 0:
             self.__balance += amt
-            print(f"Deposited {amt}. New Balance is {self.__balance}")
+            print(f"Deposited {amt}. New Balance = {self.__balance}")
         else:
-            print("Invalid Deposit Amount")
-
-    def set_balance(self,new_Bal):
-        self.__balance = new_Bal
+            print("Invalid amount")
 
     def get_balance(self):
-       return self.__balance
-    
+        return self.__balance
+
+    def set_balance(self, bal):
+        self.__balance = bal
+
+# account display -> 
     def __str__(self):
-        print(f"Account Number : [{self.Account_number}] - Owner : [{self.owner}] - Balance : [{self.__balance}]")
-       
+        return f"Account No: {self.account_number}, Owner: {self.owner.name}, Balance: {self.__balance}"
 
-#Inheritance + Polymorphism
-class SavingAcc(Account): # 2000 -> 1500
-
-    def withdraw(self,amt): # 500
-        if amt <= self.get_balance():
-            self.set_balance(self.get_balance - amt) # set (2000 - 500)
-            print(f"Withdraw {amt} , New Balance = {self.get_balance()}")
-        else:
-            print("Insufficient Funds in the Savings Account")
-
-
-class CurrentAccount(Account):
-    def __init__(self, Account_number, owner, balance=0 , odLimit = 5000): # current k liye
-        super().__init__(Account_number, owner, balance) # rest account se lekr aagya 
-        self.odLimit = odLimit
-
-
-    def withdraw(self,amt):
-        if amt <= self.get_balance() + self.odLimit:
+# inheritance + Polymorphism
+class SavingAcc(Account):
+    def withdraw(self, amt):
+        if amt <= 0:
+            print("Invalid amount")
+        elif amt <= self.get_balance():
             self.set_balance(self.get_balance() - amt)
-            print(f"Withdraw {amt} , New Balance = {self.get_balance()}")
+            print(f"Withdrawn {amt}. Balance = {self.get_balance()}")
         else:
-            print("Insufficient Funds in the Current  Account")
+            print("Insufficient Balance")
 
+# inheritance + polymorphism
+class CurrentAccount(Account):
+    def __init__(self, acc_no, owner, balance=0, od_limit=5000):
+        super().__init__(acc_no, owner, balance)
+        self.od_limit = od_limit
+
+    def withdraw(self, amt):
+        if amt <= 0:
+            print("Invalid amount")
+        elif amt <= self.get_balance() + self.od_limit:
+            self.set_balance(self.get_balance() - amt)
+            print(f"Withdrawn {amt}. Balance = {self.get_balance()}")
+        else:
+            print("Overdraft limit exceeded")
 
 class Customer:
-    def __init__(self , name , customer_id):
+    def __init__(self, name, customer_id):
         self.name = name
         self.customer_id = customer_id
 
 
-    def __str__(self):
-        return f"Customer : [{self.name}] - Id : [{self.customer_id}]"
-    
-
+#main 
 class Bank:
-    def __init__(self , filename = "accounts.txt"):
-        self.accounts = {}
-        self.filename = filename
-        self.load_account()
+    def __init__(self, filename="accounts.txt"):
+        self.accounts = {} # entry hone ke baad data accounts ke andr aaega 
+        self.filename = filename # filename ke andr store 
+        self.load_accounts() # load account 
 
+    def create_account(self, acc_type, acc_no, owner):
+        if acc_no in self.accounts:
+            print("Account already exists!")
+            return
 
-    def createAccount(self, Account_type , account_number , owner):
-        if Account_type == "Savings":
-            account = SavingAcc(account_number, owner)
-
-        elif Account_type == "Current":
-            account = CurrentAccount(account_number,owner)
-
+        if acc_type == "Savings":
+            acc = SavingAcc(acc_no, owner)
+        elif acc_type == "Current":
+            acc = CurrentAccount(acc_no, owner)
         else:
             print("Invalid Account Type")
             return
 
-        self.accounts[account_number] = account
+        self.accounts[acc_no] = acc
         self.save_accounts()
-        print(f"{Account_type.captialize()} account is created for {owner.name}")
+        print(f"{acc_type} Account created successfully")
 
-    def get_account(self,account_number):
-            return self.accounts.get(account_number, None)
-        
+    def get_account(self, acc_no):
+        return self.accounts.get(acc_no)
+
     def save_accounts(self):
-            with open(self.filename,"w") as f:
-                for acc in self.accounts.values():
-                    f.write(f"{acc.Account_type}, {acc.account_number}, {acc.owner.name},{acc.owner.customer_id},{acc.getbalance()}\n")
+        with open(self.filename, "w") as f:
+            for acc in self.accounts.values():
+                acc_type = acc.__class__.__name__
+                f.write(f"{acc_type}|{acc.account_number}|{acc.owner.customer_id}|{acc.owner.name}|{acc.get_balance()}\n")
 
-    def load_account(self):
-            if not os.path.exists(self.filename):
-                return
-            with open(self.filename, "r") as f:
-                for line in f:
-                    acc_type , acc_no , name , cust_id , balance = line.strip(),split(",")
-                    owner = Customer(name,int(cust_id))
+    def load_accounts(self):
+        if not os.path.exists(self.filename):
+            return
 
-                    if acc_type == "Savings":
-                        acc = SavingAcc(int(acc_no), owner , float(balance))
+        with open(self.filename, "r") as f:
+            for line in f:
+                try:
+                    acc_type, acc_no,cid, name , bal = line.strip().split("|")
+                    owner = Customer(name, int(cid))
+
+                    if acc_type == "SavingAcc":
+                        acc = SavingAcc(int(acc_no), owner, float(bal))
                     else:
-                        acc = CurrentAccount(int(acc_no), owner , float(balance))
+                        acc = CurrentAccount(int(acc_no), owner, float(bal))
 
-                    self.accounts[int[acc_no]] = acc
+                    self.accounts[int(acc_no)] = acc
+                except:
+                    continue
 
 
-# ----- DEMO -----
-if __name__ == "__main__": # all class ko ek sath run krne ka tarika 
+# starter -> module , background executing files unko woh run krdega -> main kaam ko execute
+if __name__ == "__main__":
     bank = Bank()
 
-
-    while(True):
-        print("\n --- BANK MENU ---")
-        print("1. Create a Savings Acc")
-        print("2. Create a Current Account")
-        print("3. Deposit Money")
-        print("4. Withdraw Money")
+    while True:
+        print("\n----- BANK MENU -----")
+        print("1. Create Savings Account")
+        print("2. Create Current Account")
+        print("3. Deposit")
+        print("4. Withdraw")
         print("5. Check Balance")
         print("6. Exit")
 
-        choice = input("Enter your Choice: ")
+        choice = input("Enter choice: ")
 
         if choice == "1":
-            name = input("Enter Your Customer Name \n")
-            c_id = input("Enter Your Customer ID \n")
-            acc_no = input("Enter Your Customer Acc No \n")
-            bank.createAccount("Savings",acc_no,Customer(name,c_id) )
-
+            name = input("Name: ")
+            cid = int(input("Customer ID: "))
+            acc = int(input("Account No: "))
+            bank.create_account("Savings", acc, Customer(name, cid))
 
         elif choice == "2":
-            name = input("Enter Your Customer Name \n")
-            c_id = input("Enter Your Customer ID \n")
-            acc_no = input("Enter Your Customer Acc No \n")
-            bank.createAccount("Current",acc_no,Customer(name,c_id) )
+            name = input("Name: ")
+            cid = int(input("Customer ID: "))
+            acc = int(input("Account No: "))
+            bank.create_account("Current", acc, Customer(name, cid))
 
         elif choice == "3":
-            acc_no = input("Enter Your Customer Acc No")
-            amount = float(input("Enter your Amount to Deposit: \n"))
-            acc = bank.get_account(acc_no)
-
-            if acc:
-                acc.deposit(amount)
-                bank.save_accounts()
+            acc = int(input("Account No: "))
+            amt = float(input("Amount: "))
+            acc_obj = bank.get_account(acc)
+            if acc_obj:
+                acc_obj.deposit(amt)
+                bank.save_accounts() # file m changes
             else:
-                print("Account Not Found")
+                print("Account not found")
 
         elif choice == "4":
-            acc_no = input("Enter Your Customer Acc No")
-            amount = float(input("Enter your Amount to Withdraw: \n"))
-
-            acc = bank.get_account(acc_no)
-
-            if(acc):
-                acc.withdraw(amount)
+            acc = int(input("Account No: "))
+            amt = float(input("Amount: "))
+            acc_obj = bank.get_account(acc)
+            if acc_obj:
+                acc_obj.withdraw(amt)
                 bank.save_accounts()
             else:
-                print("Account Not Found")
+                print("Account not found")
 
-            
         elif choice == "5":
-             acc_no = input("Enter Your Customer Acc No")
-             acc = bank.get_account(acc_no)
-
-             if acc:
-                 print(f"Balance for Account {acc_no} = {acc.getBalance()}")
-             else:
-                 print("Account Not Found")
-
+            acc = int(input("Account No: "))
+            acc_obj = bank.get_account(acc)
+            if acc_obj:
+                print("Balance:", acc_obj.get_balance())
+            else:
+                print("Account not found")
 
         elif choice == "6":
-            print("Exiting... Thank Your For Banking with us!!!!")
+            print("Thank you for banking!")
             break
 
         else:
-            print("Invalid Choice - Please Try Again")
+            print("Invalid choice")
